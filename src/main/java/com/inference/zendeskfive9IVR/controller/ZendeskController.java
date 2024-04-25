b22f8532be47600064944d9c2f59dc550bfe2201
Joel Anthony Arauzo Masgo <jarauzo@wssperu.com.pe>
2024-04-25 02:31:59 -0500

    Tercer avance configuraciones

--- src/main/java/com/inference/zendeskfive9IVR/controller/ZendeskController.java
+++ src/main/java/com/inference/zendeskfive9IVR/controller/ZendeskController.java
@@ -44,20 +44,20 @@ public class ZendeskController {
         List<String> createdAtValues = new ArrayList<>();
         DateTimeFormatter originalFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
         DateTimeFormatter targetFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        logger.info("Info message []  "+itemsNode);
+        logger.info("Info message []  " + itemsNode);
         if (!itemsNode.isEmpty()) {
-            logger.info("Info message success register find id [] "+itemsNode);
+            logger.info("Info message success register find id [] " + itemsNode);
             for (JsonNode item : itemsNode) {
                 JsonNode dataNode = item.get("data");
                 String createdAt = dataNode.get("created_at").asText();
-                logger.info("Info message success register find id object [] "+dataNode);
+                logger.info("Info message success register find id object [] " + dataNode);
                 LocalDateTime createdAtDateTime = LocalDateTime.parse(createdAt, originalFormatter);
                 LocalDateTime adjustedCreatedAtDateTime = createdAtDateTime.minusHours(5); // Restar 5 horas
                 String formattedCreatedAt = adjustedCreatedAtDateTime.format(targetFormatter);
                 createdAtValues.add(formattedCreatedAt);
             }
             if (requestNubyx.getId().length() >= 8 && requestNubyx.getId().length() <= 11) {
-                if(!requestNubyx.getId().isEmpty()){
+                if (!requestNubyx.getId().isEmpty()) {
                     for (String date : createdAtValues) {
                         SimpleDateFormat originalDateFormat = new SimpleDateFormat("yyyy-MM-dd");
                         Date formattedDate = originalDateFormat.parse(date);
@@ -65,27 +65,27 @@ public class ZendeskController {
                         String formattedCurrentDateStr = originalDateFormat.format(currentDate);
                         Date formattedCurrentDate = originalDateFormat.parse(formattedCurrentDateStr);
                         if (formattedCurrentDate.equals(formattedDate)) {
-                            logger.info("Error message in same day [] "+date);
+                            logger.info("Error message in same day [] " + date);
                             return ResponseEntity.badRequest().body(new MessageSucces("unregistered", null));
                         }
                         if (formattedCurrentDate.after(formattedDate)) {
-                                logger.info("Info message success register after day [] "+date);
-                                zendeskService.createZendesk(requestNubyx.getId(), requestNubyx.getNumber(), requestNubyx.getChannel());
-                                return ResponseEntity.ok(new MessageSucces("registered", requestNubyx.getId()));
+                            logger.info("Info message success register after day [] " + date);
+                            zendeskService.createZendesk(requestNubyx.getId(), requestNubyx.getNumber(), requestNubyx.getChannel());
+                            return ResponseEntity.ok(new MessageSucces("registered", requestNubyx.getId()));
                         }
                     }
                 }
             } else {
-                logger.info("Error message in length id find [] "+itemsNode);
+                logger.info("Error message in length id find [] " + itemsNode);
                 return ResponseEntity.badRequest().body(new MessageSucces("longitud de id no valido", null));
             }
         } else {
-            if(requestNubyx.getId().length() >= 8 && requestNubyx.getId().length() <= 11){
-                logger.info("Info message success register first [] "+requestNubyx.getId());
+            if (requestNubyx.getId().length() >= 8 && requestNubyx.getId().length() <= 11) {
+                logger.info("Info message success register first [] " + requestNubyx.getId());
                 zendeskService.createZendesk(requestNubyx.getId(), requestNubyx.getNumber(), requestNubyx.getChannel());
                 return ResponseEntity.ok(new MessageSucces("registered", requestNubyx.getId()));
-            }else{
-                logger.info("Error message in length id first [] "+itemsNode);
+            } else {
+                logger.info("Error message in length id first [] " + itemsNode);
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new MessageSucces("longitud de id no valido", null));
             }
         }
