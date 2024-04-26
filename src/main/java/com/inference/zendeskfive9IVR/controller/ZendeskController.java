677013b691ac59d009ca16f5c4c0c4669a4e0c38
Joel Anthony Arauzo Masgo <jarauzo@wssperu.com.pe>
2024-04-26 13:14:02 -0500

    Octavo avance configuraciones

--- src/main/java/com/inference/zendeskfive9IVR/controller/ZendeskController.java
+++ src/main/java/com/inference/zendeskfive9IVR/controller/ZendeskController.java
@@ -1,25 +1,16 @@
 package com.inference.zendeskfive9IVR.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inference.zendeskfive9IVR.model.MessageSucces;
 import com.inference.zendeskfive9IVR.model.RequestNubyx;
 import com.inference.zendeskfive9IVR.service.ZendeskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -32,76 +23,11 @@ public class ZendeskController {
 
     private static final Logger logger = LogManager.getLogger(ZendeskController.class);
 
-    private boolean validationExecuted = false;
-
     @PostMapping("/lead")
     public ResponseEntity<?> principal(
             @RequestBody RequestNubyx requestNubyx
     ) throws JsonProcessingException, ParseException {
-        ResponseEntity<String> prueba = zendeskService.listZendesk(requestNubyx.getId());
-        String responseBody = prueba.getBody();
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(responseBody);
-        JsonNode itemsNode = jsonNode.get("items");
-        List<String> createdAtValues = new ArrayList<>();
-        DateTimeFormatter originalFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
-        DateTimeFormatter targetFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        logger.info("Info message init[]  ");
-        if (!itemsNode.isEmpty()) {
-            logger.info("Info message success register find id [] " + itemsNode);
-            for (JsonNode item : itemsNode) {
-                JsonNode dataNode = item.get("data");
-                String createdAt = dataNode.get("created_at").asText();
-                logger.info("Info message success register find id object [] " + dataNode);
-                LocalDateTime createdAtDateTime = LocalDateTime.parse(createdAt, originalFormatter);
-                LocalDateTime adjustedCreatedAtDateTime = createdAtDateTime.minusHours(5); // Restar 5 horas
-                String formattedCreatedAt = adjustedCreatedAtDateTime.format(targetFormatter);
-                createdAtValues.add(formattedCreatedAt);
-            }
-            if (requestNubyx.getId().length() >= 8 && requestNubyx.getId().length() <= 11) {
-                if (!requestNubyx.getId().isEmpty()) {
-                    for (String date : createdAtValues) {
-                        SimpleDateFormat originalDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                        Date formattedDate = originalDateFormat.parse(date);
-                        Date currentDate = new Date();
-                        String formattedCurrentDateStr = originalDateFormat.format(currentDate);
-                        Date formattedCurrentDate = originalDateFormat.parse(formattedCurrentDateStr);
-                        if (formattedCurrentDate.equals(formattedDate)) {
-                            logger.info("Error message in same day [] " + date);
-                            return ResponseEntity.badRequest().body(new MessageSucces("unregistered", null));
-                        }
-                        if (formattedCurrentDate.after(formattedDate)  && !validationExecuted) {
-                            logger.info("Info message success register after day [] " + date);
-                            ResponseEntity<String> zendesksecond=zendeskService.createZendesk(requestNubyx.getId(), requestNubyx.getNumber(), requestNubyx.getChannel());
-                            String responseBodysecond = zendesksecond.getBody();
-                            ObjectMapper objectMappersecond = new ObjectMapper();
-                            JsonNode jsonNodesecond = objectMappersecond.readTree(responseBodysecond);
-                            JsonNode dataNodesecond = jsonNodesecond.get("data");
-                            String idcreatesecond = dataNodesecond.get("id").asText();
-                            validationExecuted = true;
-                            return ResponseEntity.ok(new MessageSucces("registered", idcreatesecond));
-                        }
-                    }
-                }
-            } else {
-                logger.info("Error message in length id find [] " + itemsNode);
-                return ResponseEntity.badRequest().body(new MessageSucces("longitud de id no valido", null));
-            }
-        } else {
-            if (requestNubyx.getId().length() >= 8 && requestNubyx.getId().length() <= 11) {
-                ResponseEntity<String> zendesk=  zendeskService.createZendesk(requestNubyx.getId(), requestNubyx.getNumber(), requestNubyx.getChannel());
-                String responseBodyfirst = zendesk.getBody();
-                ObjectMapper objectMapperfirst = new ObjectMapper();
-                JsonNode jsonNodefirst = objectMapperfirst.readTree(responseBodyfirst);
-                JsonNode dataNodefirst = jsonNodefirst.get("data");
-                String idcreatefirst = dataNodefirst.get("id").asText();
-                return ResponseEntity.ok(new MessageSucces("registered", idcreatefirst));
-            } else {
-                logger.info("Error message in length id first [] " + itemsNode);
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new MessageSucces("longitud de id no valido", null));
-            }
-        }
-        logger.info("Error message in server []");
-        return null;
+        ResponseEntity<MessageSucces> response=zendeskService.createZendesklis(requestNubyx);
+        return response;
     }
 }
--- src/main/java/com/inference/zendeskfive9IVR/service/ZendeskService.java
+++ src/main/java/com/inference/zendeskfive9IVR/service/ZendeskService.java
@@ -1,20 +1,39 @@
 package com.inference.zendeskfive9IVR.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.inference.zendeskfive9IVR.model.MessageSucces;
+import com.inference.zendeskfive9IVR.model.RequestNubyx;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class ZendeskService {
 
     private static final Logger logger = LogManager.getLogger(ZendeskService.class);
+
+    private static Date lastActionDate = null;
+    private boolean validationExecuted = false;
+    private Date lastValidationDate = null;
+
+    private Date lastRegistrationDate = null;
 
     @Value("${token.access.get}")
     private String token;
@@ -71,5 +90,77 @@ public class ZendeskService {
         HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
         ResponseEntity<String> responseprueba=restTemplate.postForEntity(url, requestEntity, String.class);
         return responseprueba;
+    }
+
+
+    public ResponseEntity<MessageSucces> createZendesklis(RequestNubyx requestNubyx) throws JsonProcessingException, ParseException {
+        ResponseEntity<String> prueba = listZendesk(requestNubyx.getId());
+        String responseBody = prueba.getBody();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(responseBody);
+        JsonNode itemsNode = jsonNode.get("items");
+        List<String> createdAtValues = new ArrayList<>();
+        DateTimeFormatter originalFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        DateTimeFormatter targetFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        logger.info("Info message init[]  ");
+        if (!itemsNode.isEmpty()) {
+            logger.info("Info message success register find id [] " + itemsNode);
+            for (JsonNode item : itemsNode) {
+                JsonNode dataNode = item.get("data");
+                String createdAt = dataNode.get("created_at").asText();
+                logger.info("Info message success register find id object [] " + dataNode);
+                LocalDateTime createdAtDateTime = LocalDateTime.parse(createdAt, originalFormatter);
+                LocalDateTime adjustedCreatedAtDateTime = createdAtDateTime.minusHours(5); // Restar 5 horas
+                String formattedCreatedAt = adjustedCreatedAtDateTime.format(targetFormatter);
+              createdAtValues.add(formattedCreatedAt);
+            }
+            if (requestNubyx.getId().length() >= 8 && requestNubyx.getId().length() <= 11) {
+                if (!requestNubyx.getId().isEmpty()) {
+                    for (String date : createdAtValues) {
+                        SimpleDateFormat originalDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                        Date formattedDate = originalDateFormat.parse(date);
+                        Date currentDate = new Date();
+                        String formattedCurrentDateStr = originalDateFormat.format(currentDate);
+                        Date formattedCurrentDate = originalDateFormat.parse(formattedCurrentDateStr);
+
+                        if (formattedCurrentDate.equals(formattedDate)) {
+                            logger.info("Error message in same day [] " + date);
+                            return ResponseEntity.badRequest().body(new MessageSucces("unregistered", null));
+                        }
+
+                        if (formattedCurrentDate.after(formattedDate)  && !validationExecuted) {
+                            // Si ya se ha registrado un lead en el día actual, devolver un mensaje de error
+                            logger.info("Info message success register after day [] " + date);
+                            ResponseEntity<String> zendesksecond = createZendesk(requestNubyx.getId(), requestNubyx.getNumber(), requestNubyx.getChannel());
+                            String responseBodysecond = zendesksecond.getBody();
+                            ObjectMapper objectMappersecond = new ObjectMapper();
+                            JsonNode jsonNodesecond = objectMappersecond.readTree(responseBodysecond);
+                            JsonNode dataNodesecond = jsonNodesecond.get("data");
+                            String idcreatesecond = dataNodesecond.get("id").asText();
+                            validationExecuted = true;
+                            return ResponseEntity.ok(new MessageSucces("registered", idcreatesecond));
+                        }
+                    }
+                }
+            } else {
+                logger.info("Error message in length id find [] " + itemsNode);
+                return ResponseEntity.badRequest().body(new MessageSucces("longitud de id no valido", null));
+            }
+        } else {
+            if (requestNubyx.getId().length() >= 8 && requestNubyx.getId().length() <= 11) {
+                ResponseEntity<String> zendesk=  createZendesk(requestNubyx.getId(), requestNubyx.getNumber(), requestNubyx.getChannel());
+                String responseBodyfirst = zendesk.getBody();
+                ObjectMapper objectMapperfirst = new ObjectMapper();
+                JsonNode jsonNodefirst = objectMapperfirst.readTree(responseBodyfirst);
+                JsonNode dataNodefirst = jsonNodefirst.get("data");
+                String idcreatefirst = dataNodefirst.get("id").asText();
+                return ResponseEntity.ok(new MessageSucces("registered", idcreatefirst));
+            } else {
+                logger.info("Error message in length id first [] " + itemsNode);
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new MessageSucces("longitud de id no valido", null));
+            }
+        }
+        logger.info("Error message in server []");
+        return null;
     }
 }
